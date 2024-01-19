6e5cd358b037626f6d8726e5a1e632e895ae0ca8
Hashem195 <hashem.w.h@gmail.com>
2024-01-19 10:03:25 +0100

    finir

--- src/jeupokemon/models/Attaque.java
+++ src/jeupokemon/models/Attaque.java
@@ -1,19 +1,75 @@
 package jeupokemon.models;
 
 public class Attaque {
-    protected String nomAttaque;
-    protected String typeAttaque;
-    protected boolean categorieAttaque;
-    protected double precisionAttaque;
-    protected int puissanceAttaque;
-    protected int ppAttaque;
-//CONSTRUCTEUR
-    public Attaque(String nomAttaque, String typeAttaque, boolean categorieAttaque, double precisionAttaque, int puissanceAttaque, int ppAttaque){
-        this.nomAttaque = nomAttaque;
-        this.typeAttaque = typeAttaque;
-        this.categorieAttaque = categorieAttaque;
-        this.precisionAttaque = precisionAttaque;
-        this.puissanceAttaque = puissanceAttaque;
-        this.ppAttaque = ppAttaque;
+    private String nom;
+    private String type;
+    private String categorie;
+    private double precision;
+    private int puissance;
+    private int pp;
+
+    // Constructor
+    public Attaque(String nom, String type, String categorie, double precision, int puissance, int pp) {
+        this.nom = nom;
+        this.type = type;
+        this.categorie = categorie;
+        this.precision = precision;
+        this.puissance = puissance;
+        this.pp = pp;
+    }
+    public String getNom() {
+        return nom;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getCategorie() {
+        return categorie;
+    }
+
+    public double getPrecision() {
+        return precision;
+    }
+
+    public int getPuissance() {
+        return puissance;
+    }
+
+    public int getPp() {
+        return pp;
+    }
+    public int calculerDegats(Pokemon attaquant, Pokemon adversaire, int attaque) {
+        double stab = 1.0;
+        if (attaquant.getType1().equals(type) || (attaquant.getType2() != null && attaquant.getType2().equals(type))) {
+            stab = 1.5;
+        }
+
+        double cm = ((2.0 * attaquant.getNiveau() / 5.0 + 2.0) * attaque * puissance) /
+                (adversaire.getDefense() * 50.0) + 2.0;
+
+
+        cm *= stab;
+
+
+        int damage = (int) (cm * (Math.random() * 0.15 + 0.85));
+
+
+        pp--;
+        if (pp < 0) {
+            pp = 0;
+        }
+
+        return damage;
+    }
+
+    public void afficher() {
+        System.out.println("Attaque : " + nom);
+        System.out.println("Type : " + type);
+        System.out.println("Catégorie : " + categorie);
+        System.out.println("Précision : " + precision);
+        System.out.println("Puissance : " + puissance);
+        System.out.println("PP : " + pp);
     }
 }
--- src/jeupokemon/models/Pokemon.java
+++ src/jeupokemon/models/Pokemon.java
@@ -1,40 +1,116 @@
 package jeupokemon.models;
 
 import java.util.List;
-
+import java.util.ArrayList;
 public class Pokemon {
-    protected String nom;
-    protected double prix;
-    protected String types;
-    protected int pointsVie;
-    protected int niveau;
-    protected int attaque;
-    protected int attaqueSpecial;
-    protected int defense;
-    protected int defenseSpecial;
-    protected int vitesse;
-    protected List<Attaque> attaques;
+    private String nom;
+    private int prix;
+    private String type1;
+    private String type2;
+    private int pointsDeVie;
+    private int niveau;
+    private int attaque;
+    private int attaqueSpeciale;
+    private int defense;
+    private int defenseSpeciale;
+    private int vitesse;
+    private List<Attaque> attaques;
 
-//Constructeur:
-    public Pokemon(String nom, double prix, String types, int pointsVie, int niveau, int attaque, int attaqueSpecial, int defense, int defenseSpecial, int vitesse, List<Attaque> attaques) {
+    // Constructor
+    public Pokemon(String nom, int prix, String type1, String type2, int pointsDeVie, int niveau,
+                   int attaque, int attaqueSpeciale, int defense, int defenseSpeciale, int vitesse) {
         this.nom = nom;
         this.prix = prix;
-        this.types = types;
-        this.pointsVie = pointsVie;
+        this.type1 = type1;
+        this.type2 = type2;
+        this.pointsDeVie = pointsDeVie;
         this.niveau = niveau;
         this.attaque = attaque;
-        this.attaqueSpecial = attaqueSpecial;
+        this.attaqueSpeciale = attaqueSpeciale;
         this.defense = defense;
-        this.defenseSpecial = defenseSpecial;
+        this.defenseSpeciale = defenseSpeciale;
         this.vitesse = vitesse;
-        this.attaques = attaques;
+        this.attaques = new ArrayList<>();
     }
 
-    public  void ajouterAttaque(Attaque attaque){
+    public String getNom() {
+        return nom;
+    }
+
+    public int getPrix() {
+        return prix;
+    }
+
+    public String getType1() {
+        return type1;
+    }
+
+    public String getType2() {
+        return type2;
+    }
+
+    public int getPointsDeVie() {
+        return pointsDeVie;
+    }
+
+    public int getNiveau() {
+        return niveau;
+    }
+
+    public int getAttaque() {
+        return attaque;
+    }
+
+    public int getAttaqueSpeciale() {
+        return attaqueSpeciale;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public int getDefenseSpeciale() {
+        return defenseSpeciale;
+    }
+
+    public int getVitesse() {
+        return vitesse;
+    }
+
+    public List<Attaque> getAttaques() {
+        return attaques;
+    }
+
+    // Methode pour ajouter attaque
+    public void ajouterAttaque(Attaque attaque) {
         attaques.add(attaque);
     }
-    public void attaquer (){
-        // logic of attaquing
+
+    public void attaquer(Pokemon adversaire, Attaque attaque) {
+        // Implement the attack logic here
+        // Use the Attaque calculerDegats method
+        // Adjust the opponent's HP accordingly
+    }
+
+    public boolean estKo() {
+        return pointsDeVie <= 0;
+    }
+
+    //pour apres
+    public void afficherAttaques() {
+        for (Attaque attaque : attaques) {
+            attaque.afficher();
+        }
+    }
+
+
+
+    public void afficher() {
+        System.out.println("Nom : " + nom);
+        System.out.println("Type(s) : " + type1 + (type2 != null ? ", " + type2 : ""));
+        System.out.println("PV : " + pointsDeVie);
+        System.out.println("Niveau : " + niveau);
+
     }
 
 }
